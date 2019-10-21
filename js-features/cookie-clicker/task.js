5c1adcf3a1266b0df0583eacb3906ed533b6759b
unknown <ybalimova@gmail.com>
2019-10-21 21:09:46 +0300

    change speed counter

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,20 +1,21 @@
 const counter = document.getElementById('clicker__counter');
 const cookie = document.getElementById('cookie');
 const speed = document.getElementById('speed');
-let start = Date.now();
-let end;
+let start = 0;
 let difference;
 
 cookie.onclick = function() {
+    let end = Date.now();
     counter.textContent++;
     if (counter.textContent % 2 === 0) {
         cookie.width = 200;
     } else {
         cookie.width = 300;
     }
-    end = Date.now();
+    
     difference = (1000 / (end - start));
     speed.textContent = difference;
+    start = Date.now();
 }
 
 
