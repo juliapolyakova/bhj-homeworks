29efc52500bfc3eba5886c7731698e6f35bacc4b
unknown <ybalimova@gmail.com>
2019-11-06 12:22:12 +0300

    task done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,24 +1,27 @@
 
 const rotator = Array.from(document.querySelectorAll('.rotator__case'));
+const caseActive = document.querySelector('.rotator__case_active');
+caseActive.style.color = caseActive.getAttribute('data-color');
 
 let i = 0;
 let interval = 1000;
 
 function showNext(){
-    const caseActive = document.querySelector('.rotator__case_active');
-    caseActive.style.color = caseActive.getAttribute('data-color');
-    
-    interval = caseActive.getAttribute('data-speed');
-    
-    rotator[i].classList.toggle('rotator__case_active');
+    rotator[i].classList.remove('rotator__case_active');
+        
     i++;
+    
     if (i === rotator.length) {
         i = 0;
     }
+    
+    rotator[i].classList.add('rotator__case_active');    
+    const caseActive = document.querySelector('.rotator__case_active');
+    caseActive.style.color = caseActive.getAttribute('data-color');
+    interval = caseActive.getAttribute('data-speed');
 
-    rotator[i].classList.toggle('rotator__case_active');    
-      
+    setTimeout(showNext, interval)
 }
 
 
-setInterval(showNext, interval)
+setTimeout(showNext, interval)
