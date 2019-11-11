8807f76d881a143ca7de498fe03e43c345b57b76
unknown <ybalimova@gmail.com>
2019-11-11 13:52:24 +0300

    task done

---
+++ document-structure/todo/task.js
@@ -0,0 +1,32 @@
+const tasksAdd = document.getElementById('tasks__add');
+const taskList = document.getElementById('tasks__list');
+
+tasksAdd.addEventListener('click', addTask);
+
+function addTask(event) {
+    event.preventDefault();
+    let text = document.getElementById('task__input');
+    if (text.value !== '') {
+
+        let newTask =  `<div class="task">
+                    <div class="task__title">
+                        ${text.value}
+                   </div>
+                   <a href="#" class="task__remove">&times;</a>
+                   </div>`;
+
+        taskList.insertAdjacentHTML('beforeEnd', newTask);
+        text.value = '';
+
+        toDeleted();
+    }
+}
+
+function toDeleted() {
+    taskList.addEventListener('click', (event) => {
+        event.preventDefault();
+        if (event.target.classList.contains('task__remove')) {    
+           event.target.parentElement.remove();
+       }
+    })
+}
